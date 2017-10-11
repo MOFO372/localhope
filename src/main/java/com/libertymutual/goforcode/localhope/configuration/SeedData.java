90bdda9a5368f220dd4bb562e75556eedb0193a7
Rachel Soley <rachel.soley@libertymutual.com>
2017-10-11 08:07:48 -0700

    added a session controller and removed more unused imports.

--- src/main/java/com/libertymutual/goforcode/localhope/configuration/SeedData.java
+++ src/main/java/com/libertymutual/goforcode/localhope/configuration/SeedData.java
@@ -1,12 +1,5 @@
 package com.libertymutual.goforcode.localhope.configuration;
 
-import java.sql.Date;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoUnit;
-import java.util.Calendar;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
---
+++ src/main/java/com/libertymutual/goforcode/localhope/controllers/SessionController.java
@@ -0,0 +1,63 @@
+package com.libertymutual.goforcode.localhope.controllers;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.libertymutual.goforcode.localhope.models.UserD;
+import com.libertymutual.goforcode.localhope.repositories.NeedRepository;
+import com.libertymutual.goforcode.localhope.repositories.UserRepository;
+
+@CrossOrigin(origins = "*")
+@Controller
+@RequestMapping("")
+public class SessionController {
+	
+	private NeedRepository needRepository;
+	private UserRepository userRepository;
+	private PasswordEncoder encoder;
+	
+	
+	// add: PasswordEncoder encoder as parameter
+	public SessionController(NeedRepository needRepository, UserRepository userRepository, PasswordEncoder encoder) {
+		this.needRepository = needRepository;
+		this.userRepository = userRepository;
+		this.encoder = encoder;
+	}
+	
+	//JSON
+		@GetMapping("registration")
+	    public ModelAndView registration() {
+	        ModelAndView mv = new ModelAndView();
+	        mv.setViewName("registration");
+	        return mv;
+	    }
+		
+		//JSON
+		@PostMapping("registration")
+		public ModelAndView register(@RequestBody UserD user, HttpServletResponse response) {
+			
+			String password = user.getPassword();
+			String encryptedPassword = encoder.encode(password);
+			user.setPassword(encryptedPassword);
+
+			ModelAndView mv = new ModelAndView();
+			try { 
+				userRepository.save(user);
+				mv.setViewName("needs");
+			} catch (DataIntegrityViolationException dive) {
+				mv.setViewName("needs");
+				mv.addObject("errorMessage", "Cannot register that username");
+			}
+			return mv;
+		}
+
+}
