18c306093fed2456ac7d1bdb3cc95bd1df5cd644
Rachel Soley <rachel.soley@libertymutual.com>
2017-10-12 08:55:12 -0700

    formatting shit.

--- src/main/java/com/libertymutual/goforcode/localhope/controllers/SessionController.java
+++ src/main/java/com/libertymutual/goforcode/localhope/controllers/SessionController.java
@@ -1,7 +1,6 @@
 package com.libertymutual.goforcode.localhope.controllers;
 
 import java.util.List;
-import java.util.Map;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -16,14 +15,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.libertymutual.goforcode.localhope.models.Need;
 import com.libertymutual.goforcode.localhope.models.UserD;
 import com.libertymutual.goforcode.localhope.repositories.NeedRepository;
 import com.libertymutual.goforcode.localhope.repositories.UserRepository;
-
-
 
 @RestController
 @CrossOrigin(origins = "*")
@@ -33,16 +28,14 @@ public class SessionController {
 	private NeedRepository needRepository;
 	private UserRepository userRepository;
 	private PasswordEncoder encoder;
-	
 
 	public SessionController(NeedRepository needRepository, UserRepository userRepository, PasswordEncoder encoder) {
 		this.needRepository = needRepository;
 		this.userRepository = userRepository;
 		this.encoder = encoder;
-		
+
 	}
 
-	
 	@GetMapping("registration")
 	public ModelAndView registration() {
 		ModelAndView mv = new ModelAndView();
@@ -50,19 +43,13 @@ public class SessionController {
 		return mv;
 	}
 
-
-
-
 	@GetMapping("dogooder")
 	public List<Need> getAllNeeds() {
 		return needRepository.findAll();
 	}
-	
 
 	@PostMapping("registration")
 	public UserD register(@RequestBody UserD user, HttpServletResponse response) {
-
-		
 
 		String password = user.getPassword();
 		String encryptedPassword = encoder.encode(password);
@@ -78,22 +65,21 @@ public class SessionController {
 			return null;
 		}
 	}
-	
 
 	@PostMapping("sessions")
 	public UserD login(@RequestBody LoginModel userLogin, HttpServletResponse response) {
 		UserD user = userRepository.findByUsername(userLogin.getUsername());
 		String username = userLogin.getUsername();
 		String password = userLogin.getPassword();
-		
+
 		if (user != null && BCrypt.checkpw(password, user.getPassword())) {
 			System.out.println("User is " + user);
 			System.out.println("Username is " + username);
 			System.out.println("Password is " + password);
 			return user;
 		} else {
-		return null;
+			return null;
 		}
 	}
-	
+
 }
--- src/main/java/com/libertymutual/goforcode/localhope/controllers/UserApiController.java
+++ src/main/java/com/libertymutual/goforcode/localhope/controllers/UserApiController.java
@@ -1,19 +1,10 @@
 package com.libertymutual.goforcode.localhope.controllers;
 
-import java.util.List;
-
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.domain.Sort.Order;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.libertymutual.goforcode.localhope.models.Need;
 import com.libertymutual.goforcode.localhope.models.ThisIsNotACharityException;
@@ -22,11 +13,6 @@ import com.libertymutual.goforcode.localhope.models.UserD;
 import com.libertymutual.goforcode.localhope.repositories.NeedRepository;
 import com.libertymutual.goforcode.localhope.repositories.UserRepository;
 
-
-import io.swagger.annotations.ApiOperation;
-
-
-
 @RestController
 @RequestMapping("user")
 public class UserApiController {
@@ -34,47 +20,49 @@ public class UserApiController {
 	private NeedRepository needRepository;
 	private UserRepository userRepository;
 	// private PasswordEncoder encoder;
-	
-	
+
 	// add: PasswordEncoder encoder as parameter
 	public UserApiController(NeedRepository needRepository, UserRepository userRepository) {
 		this.needRepository = needRepository;
 		this.userRepository = userRepository;
 	}
 
-	
-	
-	@PostMapping("{userid}/need")
-	public UserD associateDogooderAndNeed(@PathVariable long userid, @RequestBody Need need){
+	// associates a need with a dogooder once button is clicked
+	@PostMapping("need/{userid}")
+	public UserD associateDogooderAndNeed(@PathVariable long userid, @RequestBody Need need) {
 		UserD user = userRepository.findOne(userid);
 		need = needRepository.findOne(need.getId());
 		user.addNeed(need);
-//		userRepository.save(user);
+		// userRepository.save(user);
 		needRepository.save(need);
 		return user;
 	}
 
-	@PostMapping("{dogooderid}/followcharity")
-	public UserD associateDogooderAndCharity(@PathVariable long dogooderid, @RequestBody UserD charity) throws ThisIsNotACharityException{
-		UserD user = userRepository.findOne(dogooderid);			
-		charity = userRepository.findOne(charity.getId());			
+	// associates the dogooder with a charity
+	@PostMapping("followcharity/{dogooderid}")
+	public UserD associateDogooderAndCharity(@PathVariable long dogooderid, @RequestBody UserD charity)
+			throws ThisIsNotACharityException {
+		UserD user = userRepository.findOne(dogooderid);
+		charity = userRepository.findOne(charity.getId());
 		user.addFollowedCharity(charity);
 		userRepository.save(user);
 		return user;
 	}
 
-	@PostMapping("{dogooderid}/defollowcharity")
-	public UserD removeDogooderAndCharity(@PathVariable long dogooderid, @RequestBody UserD charity) 
-			     throws ThisIsNotACharityException, UnableToDeFollowThisCharityException{
-		UserD user = userRepository.findOne(dogooderid);			
-		charity = userRepository.findOne(charity.getId());			
+	// allows a user to unfollow a charity after being associated with it - will
+	// remove the association
+	@PostMapping("unfollowcharity/{dogooderid}")
+	public UserD removeDogooderAndCharity(@PathVariable long dogooderid, @RequestBody UserD charity)
+			throws ThisIsNotACharityException, UnableToDeFollowThisCharityException {
+		UserD user = userRepository.findOne(dogooderid);
+		charity = userRepository.findOne(charity.getId());
 		user.removeFollowedCharity(charity);
 		userRepository.save(user);
 		return user;
 	}
-	
+
 	@PostMapping("")
 	public UserD createUser(@RequestBody UserD user) {
-		return userRepository.save(user);	
-	}	
+		return userRepository.save(user);
+	}
 }
