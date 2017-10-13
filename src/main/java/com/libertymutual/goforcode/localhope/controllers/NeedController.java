cc6ae86e6bccb0e4e90f85bfba6054dc799e2ca8
Jasmine Stotts <Jasmine.Stotts@LibertyMutual.com>
2017-10-13 11:13:23 -0700

    voided the needstatus path- now returns nothing

--- src/main/java/com/libertymutual/goforcode/localhope/controllers/NeedController.java
+++ src/main/java/com/libertymutual/goforcode/localhope/controllers/NeedController.java
@@ -31,13 +31,11 @@ public class NeedController {
 
 	// Change the needMet status of a Need to the opposite  
 	@PostMapping("needstatus/{needid}") 
-	public List<Need> resetNeedMetStatus(@PathVariable long needid, @RequestBody UserD user) {				
+	public void resetNeedMetStatus(@PathVariable long needid, @RequestBody UserD user) {				
 		Need need = needRepository.findOne(needid);		
 		user = userRepository.findOne(user.getId());
 		
 		need.setNeedMet(!need.getNeedMet());			
 		need = needRepository.save(need);
-
-		return user.getNeeds();
 	}
 }
