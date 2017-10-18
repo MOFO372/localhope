54d2528920fe4ef3662553abe302f7ba90a4cff2
Rachel Soley <rachel.soley@libertymutual.com>
2017-10-18 14:17:05 -0700

    cleanup during code review.

--- src/main/java/com/libertymutual/goforcode/localhope/models/FollowUniqueCharitiesOnlyException.java
+++ src/main/java/com/libertymutual/goforcode/localhope/models/FollowUniqueCharitiesOnlyException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 
-@ResponseStatus(value=HttpStatus.NOT_FOUND, reason = "This Charity is already followed by this DoGooder.") 
+@ResponseStatus(value=HttpStatus.CONFLICT, reason = "This Charity is already followed by this DoGooder.") 
 public class FollowUniqueCharitiesOnlyException extends Exception {
 
 		private static final long serialVersionUID = 1L;	
