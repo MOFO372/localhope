b2e9d9a6cf0cc51169c406f3eacff56b2242adcc
Rachel Soley <rachel.soley@libertymutual.com>
2017-10-18 14:55:30 -0700

    succeddfully sent a test email using send grid controller code.

--- src/main/java/com/libertymutual/goforcode/localhope/controllers/SendGridController.java
+++ src/main/java/com/libertymutual/goforcode/localhope/controllers/SendGridController.java
@@ -3,7 +3,15 @@ package com.libertymutual.goforcode.localhope.controllers;
 import com.sendgrid.*;
 import java.io.IOException;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("sendgrid")
 public class SendGridController {
+	
+	@PostMapping("")
 	public static void main(String[] args) throws IOException {
 		Email from = new Email("rsoley92@gmail.com");
 		String subject = "Sending with SendGrid is Fun";
@@ -11,7 +19,7 @@ public class SendGridController {
 		Content content = new Content("text/plain", "and easy to do anywhere, even with Java");
 		Mail mail = new Mail(from, subject, to, content);
 
-		SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
+		SendGrid sg = new SendGrid("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");
 		Request request = new Request();
 		try {
 			request.setMethod(Method.POST);
