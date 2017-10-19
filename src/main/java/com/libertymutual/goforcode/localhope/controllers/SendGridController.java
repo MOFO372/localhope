5868dda266750860482e873d833049577cc9649a
Rachel Soley <rachel.soley@libertymutual.com>
2017-10-19 10:57:01 -0700

    changed sendgrid stuff.

--- src/main/java/com/libertymutual/goforcode/localhope/controllers/SendGridController.java
+++ src/main/java/com/libertymutual/goforcode/localhope/controllers/SendGridController.java
@@ -26,7 +26,7 @@ public class SendGridController {
 	@PostMapping("")
 	public void main(String username) throws IOException {
 		UserD user = userRepository.findByUsername(username);
-		Email from = new Email("rsoley92@gmail.com");
+		Email from = new Email("localhope17@gmail.com");
 		String subject = "Welcome to LocalHope!";
 		Email to = new Email(user.getEmail());
 		Content content = new Content("text/html", " ");
@@ -42,37 +42,6 @@ public class SendGridController {
 
 		SendGrid sg = new SendGrid(key);
 		
-		Request request = new Request();
-		try {
-			request.setMethod(Method.POST);
-			request.setEndpoint("mail/send");
-			request.setBody(mail.build());
-			Response response = sg.api(request);
-			System.out.println(response.getStatusCode());
-			System.out.println(response.getBody());
-			System.out.println(response.getHeaders());
-		} catch (IOException ex) {
-			throw ex;
-		}
-	}
-	
-	public void retrieve(String username) throws IOException {
-		UserD user = userRepository.findByUsername(username);
-		Email from = new Email("rsoley92@gmail.com");
-		String subject = "Your LocalHope password";
-		Email to = new Email(user.getEmail());
-		Content content = new Content("text/html", " ");
-		Mail mail = new Mail(from, subject, to, content);
-		String dumbTemplate = "89de9d75-6e04-44d1-a11d-eaba98301eb9";
-		mail.setTemplateId(dumbTemplate);
-		
-		Personalization personalization = new Personalization();
-		personalization.addTo(to);
-		personalization.addSubstitution("%first_name%", user.getFirstName());
-		
-		mail.addPersonalization(personalization);
-
-		SendGrid sg = new SendGrid("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");
 		Request request = new Request();
 		try {
 			request.setMethod(Method.POST);
